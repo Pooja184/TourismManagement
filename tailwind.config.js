b176716e253bc09156c7dd9c250b0113596438c2
pxxja <poojabhambid2004@gmail.com>
2025-07-21 22:15:54 +0530

    fix color pallete for webiste

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFB347",     // Sunset Gold   Buttons, headings, main actions
+        secondary: "#2C5F2D",   // Forest Green   Hover bg, footers, accents
+        accent: "#386FA4",      // River Blue     Link hovers, icons, decorations
+        neutral: "#F5EFE6",     // Mist Beige     Backgrounds, body text, cards
+      },
+       backgroundImage: {
+        'hero-img': "url('/images/hero.jpg')", 
+      },
+    },
   },
   plugins: [],
 }
