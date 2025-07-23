4016dc2a0ac608654789933838e2ec0af868afed
pxxja <poojabhambid2004@gmail.com>
2025-07-23 12:08:57 +0530

    Experience Categories page ui

---
+++ src/components/ExperienceCategories.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { FaUmbrellaBeach, FaHiking, FaMountain, FaWater } from "react-icons/fa";
+import { GiTempleGate } from "react-icons/gi";
+import Heading from "./Heading";
+import ExperienceCard from "./ExperienceCard";
+
+const categories = [
+  { title: 'Beaches', icon: <FaUmbrellaBeach />, bgColor: 'bg-teal-600', path: '/beaches' },
+  { title: 'Trekking', icon: <FaHiking />, bgColor: 'bg-green-600', path: '/trekking' },
+  { title: 'Temples', icon: <GiTempleGate />, bgColor: 'bg-orange-600', path: '/temples' },
+  { title: 'Mountains', icon: <FaMountain />, bgColor: 'bg-purple-600', path: '/mountains' },
+  { title: 'Waterfalls', icon: <FaWater />, bgColor: 'bg-blue-600', path: '/waterfalls' },
+];
+
+
+const ExperienceCategories = () => {
+  return (
+    <section className="py-14 px-4 min-h-screen md:px-20 bg-white">
+      <Heading
+        title="Experience Categories"
+        subtitle="Find your next adventure by choosing a category that fits your interest."
+      />
+
+       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 mt-8 md:mt-14">
+        {categories.map((item, index) => (
+          <Link to={item.path} key={index}>
+            <ExperienceCard
+              title={item.title}
+              icon={item.icon}
+              bgColor={item.bgColor}
+            />
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ExperienceCategories;
