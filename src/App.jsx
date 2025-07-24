6fe5c173a6a257fe595e245735b40a4fece3a89c
pxxja <poojabhambid2004@gmail.com>
2025-07-24 22:26:04 +0530

    Define Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Destinations from './pages/Destinations'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-      hello
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/destinations' element={<Destinations/>}/>
+   </Routes>
+   <Footer/>
     </div>
   )
 }
 
 export default App
+
